6c3464329dfea3cca78ed17f73211caaac1a87a3
wangpinhong <wangpinhong@gmail.com>
2017-02-07 22:33:05 +0800

    Half

---
+++ js/custom_js.js
@@ -0,0 +1,7 @@
+
+function showAlbums() {
+    document.getElementById("c_ul").style.visibility="visible";
+}
+function hideAlbums() {
+    document.getElementById("c_ul").style.visibility="hidden";
+}
